52a4267c29c42095f14e96e7eed9bffb308e5c32
brunEdo <edobruno.m@gmail.com>
2021-08-31 10:23:46 +0200

    error input management

--- js/app.js
+++ js/app.js
@@ -1,70 +1,91 @@
 // TICKET PRICE CALCULATOR
-const ticketPrice = () => {
-    // 1. Passenger data
+
+// 0. Store price multiplier, minor discount and over 65 discount
+const priceMultiplier = 0.21;
+const minorDiscount = 0.2;
+const over65Discount = 0.4;
+// 0. diclare variable
+let userName, userSurname, userAge, distance;
+
+// 1. Passenger data
+const storeUserData = () => {
     // 1a. store passenger name
-    let userName = document.getElementById('user-name').value;
+    userName = document.getElementById('user-name').value;
     // 1a. store passenger surname
-    let userSurname = document.getElementById('user-surname').value;
+    userSurname = document.getElementById('user-surname').value;
     // 1c. store passenger age
-    let userAge = parseInt(document.getElementById('user-age').value);
+    userAge = parseInt(document.getElementById('user-age').value);
     // 1d. store distance of the destination
-    let distance = parseInt(document.getElementById('distance').value);
+    distance = parseInt(document.getElementById('distance').value);
 
     // 2. Check for input error
-    // 2a. check valid name
-    // 2b. check valid surname
+    let validInput = true;
+    // 2a. reset error messages
+    document.getElementById('name-error').innerHTML = "";
+    document.getElementById('surname-error').innerHTML = "";
+    document.getElementById('age-error').innerHTML = "";
+    document.getElementById('distance-error').innerHTML = "";
+    // 2a. check user name
+    if (userName === "") {
+        validInput = false;
+        document.getElementById('name-error').innerHTML = "(Name is mandatory)";
+    } 
+    // 2b. check user surname
+    if (userSurname === "") {
+        validInput = false;
+        document.getElementById('surname-error').innerHTML = "(Surname is mandatory)";
+    }
     // 2c. check valid age
     if (userAge < 1 || userAge > 135 || isNaN(userAge)) {
+        validInput = false;
         document.getElementById('age-error').innerHTML = "(Invalid age, retry)";
-        ticketPrice();
-    } else {
-        document.getElementById('age-error').innerHTML = "";
-        // 2d. check valid distance
-        if (distance < 1 || isNaN(distance)) {
-            document.getElementById('distance-error').innerHTML = "(Invalid distance, retry)";
-        } else {
-            document.getElementById('distance-error').innerHTML = "";
-
-            // 3. Store price multiplier, minor discount and over 65 discount
-            const priceMultiplier = 0.21;
-            const minorDiscount = 0.2;
-            const over65Discount = 0.4;
-        
-            // 4. Calculate base price
-            let price = distance * priceMultiplier;
-            // 4a. if user is a minor apply discount
-            if (userAge < 18) {
-                price *= (1 - minorDiscount);
-                document.getElementById('discount').innerHTML = "Minors Discount";
-            // 4b. if user age is over 65 apply discount
-            } else if (userAge >= 65) {
-                price *= (1 - over65Discount);
-                document.getElementById('discount').innerHTML = "Silver Discount";
-            // 4c. if user age is between 18 and 65 no discount
-            } else {
-                document.getElementById('discount').innerHTML = "None";
-            }
-            // 4d. round the price to decimal number
-            price = price.toFixed(2);
-            // 4e. show price to the user
-            document.getElementById('ticket-price').innerHTML = price + "€";
-        
-            // 5. Print passenger full name
-            document.getElementById('full-name').innerHTML = userName + " " + userSurname;
-        
-            // 6. Calculate random Carriege
-            document.getElementById('carriege').innerHTML = Math.floor(Math.random() * 9) + 1;
-        
-            // 6. Calculate random CP code
-            document.getElementById('cp-code').innerHTML = Math.floor(Math.random() * 10000) + 90000;
-        
-            // 7. Show ticket
-            document.getElementById('ticket').classList.toggle("hide");
-        }
+    }
+    // 2d. check valid distance
+    if (distance < 1 || isNaN(distance)) {
+        validInput = false;
+        document.getElementById('distance-error').innerHTML = "(Invalid distance, retry)";
+    }
+    if (validInput == true) {
+        printTicket();
     }
 }
 
-document.getElementById('submitBtn').addEventListener('click', ticketPrice);
+// Print ticket
+const printTicket = () => {
+    // 4. Calculate base price
+    let price = distance * priceMultiplier;
+    // 4a. if user is a minor apply discount
+    if (userAge < 18) {
+        price *= (1 - minorDiscount);
+        document.getElementById('discount').innerHTML = "Minors Discount";
+    // 4b. if user age is over 65 apply discount
+    } else if (userAge >= 65) {
+        price *= (1 - over65Discount);
+        document.getElementById('discount').innerHTML = "Silver Discount";
+    // 4c. if user age is between 18 and 65 no discount
+    } else {
+        document.getElementById('discount').innerHTML = "None";
+    }
+    // 4d. round the price to decimal number
+    price = price.toFixed(2);
+    // 4e. show price to the user
+    document.getElementById('ticket-price').innerHTML = price + "€";
+    
+    // 5. Print passenger full name
+    document.getElementById('full-name').innerHTML = userName + " " + userSurname;
+    
+    // 6. Calculate random Carriege
+    document.getElementById('carriege').innerHTML = Math.floor(Math.random() * 9) + 1;
+    
+    // 6. Calculate random CP code
+    document.getElementById('cp-code').innerHTML = Math.floor(Math.random() * 10000) + 90000;
+    
+    // 7. Show ticket
+    document.getElementById('ticket').classList.toggle("hide");
+}
+        
+
+document.getElementById('submitBtn').addEventListener('click', storeUserData);
 
 const reset = () => {
     // 8. Delete input value
